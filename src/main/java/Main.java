5c3d59ba2bac4af4b281750a17db4590feb7909c
kasiadabek <Xlibellulex@gmail.com>
2021-06-20 13:06:09 +0200

    commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,12 @@
+import automation.BankAccount;
+
 public class Main {
     public static void main(String[] args) {
         BankAccount Super = new BankAccount();
-        Super.transfer(50);
-        Super.withdraw(20);
+        Super.transfer(1000);
+        Super.withdraw(700);
         Super.transfer(200);
         Super.getAccountBalance();
-        Super.withdraw(50);
+        Super.withdraw(3000);
     }
 }
